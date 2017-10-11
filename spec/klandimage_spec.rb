b2a855ebd8175b63fcd15d5af6c06c4eb9d35fd8
Alex Craik <ajc3860@gmail.com>
2017-10-11 13:08:50 -0400

    Update rspecs

--- spec/klandimage_spec.rb
+++ spec/klandimage_spec.rb
@@ -1,11 +1,15 @@
-require "spec_helper"
+require 'spec_helper'
 
-describe Klandimage do
-  it "has a version number" do
-    expect(Klandimage::VERSION).not_to be nil
+describe KlandImage do
+  it 'has a version number' do
+    expect(KlandImage::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'gets image lists' do
+    response = KlandImage.list(bucket: 'snail_screen', ipp: 5)
+    expect(response[:bucket]).to eq('snail_screen')
+    expect(response[:page]).to eq(1)
+    expect(response[:ipp]).to eq(5)
+    expect(response[:images].size).to eq(5)
   end
 end
