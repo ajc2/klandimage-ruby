2f5cdd67aa13f71daf8cbd9dda504f8e8c2a1b81
Alex Craik <ajc3860@gmail.com>
2017-10-11 12:55:47 -0400

    Fixes and updates.

--- lib/klandimage.rb
+++ lib/klandimage.rb
@@ -2,14 +2,19 @@ require "klandimage/version"
 require 'rest-client'
 require 'json'
 
+# This module contains all the request methods
 module KlandImage
+  # [Integer] Specify you want all images in the bucket.
   ALL = -1
 
   # Upload an image to kland.
   #
   # @param image [File] An image file to upload.
   # @param bucket [String] The bucket to upload to.
+  # 
   # @return [String] The URL of the uploaded image.
+  # 
+  # @raise [RuntimeError] if the POST response is not a URL.
   def self.upload(image:, bucket: '')
     response = RestClient.post(
       'https://kland.smilebasicsource.com/uploadimage',
@@ -23,21 +28,27 @@ module KlandImage
     end
   end
 
+  # Get a page of images from a bucket.
+  #
+  # @param bucket [String] The name of the bucket to check.
+  # @param page [Integer] The page of results to get.
+  #   This value doesn't matter if ipp is KlandImage::ALL (-1).
+  # @param ipp [Integer] Images per page.
+  #   The number of images retrieved will never be greater
+  #   than this value, unless using KlandImage::ALL (-1),
+  #   in which case all images in the bucket will be returned
+  #   regardless of page number.
+  # 
+  # @return [Hash{Symbol => String, Integer, Array}]
+  #   A Hash containing information about the
+  #   request, and the page of images you requested.
   def self.list(bucket: '', page: 1, ipp: 20)
     response = RestClient.get(
       'https://kland/smilebasicsource.com/image',
       {bucket: bucket, page: page, ipp: ipp, asJSON: true}
     )
 
-    # awful hack to prevent php garbage
-    start = response =~ /{/
-    raise response unless start
-    response = response[start..-1]
-
-    obj = JSON.parse(response)
-
-    # eliminate potential name garbage
-    obj['bucket'] = bucket
+    obj = JSON.parse(response, symbolize_keys: true)
 
     return obj
   end
